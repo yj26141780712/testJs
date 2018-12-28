a7c532b586efa75ac6d8745d3c5be58a9eacfb0a
yangjie <261417807@qq.com>
2018-12-28 15:25:47 +0800

    引入1.83underscore源文件

--- underscore.js
+++ underscore.js
@@ -1,7 +1,10 @@
 (function(root) {
-    let _ = function() {
-
+    let _ = function(obj) {
+        if (obj instanceof _) return obj; // obj是 _的实例对象返回obj
+        if (!(this instanceof _)) return new _(obj); // this不是 _的实例对象 返回 _的实例对象
+        console.log(obj);
+        console.log(123);
+        this._wrapped = obj;
     }
-
     root._ = _;
 })(this)
